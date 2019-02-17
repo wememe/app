7d988a06ddd75cdd28f9f88fef26bf81323f3a34
Zach Ferland <zachferland@gmail.com>
2019-02-16 21:29:24 -0700

    feat: add background to draw, fix cross origin image

--- src/views/Draw.jsx
+++ src/views/Draw.jsx
@@ -21,10 +21,10 @@ class Draw extends Component {
       shareValue: 0,
       brushRadius: 10,
       lazyRadius: 12,
-      memeId: this.props.history.location.pathname.split('/')[2]
-      // TODO maybe some cross origin issues here to work out
-      // imgSrc:"https://ipfs.infura.io/ipfs/Qmci55ieQdt8qZv5B9KuTotnsuqSUKMdmMPmjsS2x5U1pB"
+      memeId: this.props.history.location.pathname.split('/')[2],
+      imgSrc: null
     };
+
   }
 
   chooseColor = (color) => {
@@ -44,10 +44,23 @@ class Draw extends Component {
     const layer2 = canvasItems[2]
     contextBase.drawImage(layer1, 0, 0);
     contextBase.drawImage(layer2, 0, 0);
-    const image = base.toDataURL("image/png");
-    console.log(image)
+    // const image = base.toDataURL("image/png");
+    const image = base.toBlob(async (res) => {
+      console.log(res)
+      const fetch = await this.saveToIpfs(res);
+      const returnedData = await fetch.json();
+      const content = `https://ipfs.infura.io/ipfs/${returnedData.Hash}`;
+      console.log(content)
+    });
+    // console.log(image)
     // TODO upload to ipfs and get hash
   }
+
+  saveToIpfs = buffer => window.fetch('https://ipfs.infura.io:5001/api/v0/add', {
+    method: 'post',
+    'Content-Type': 'multipart/form-data',
+    body: buffer,
+  });
 
   onColorChange = (obj) => {
     this.setState({ color: obj.hex })
@@ -69,29 +82,78 @@ class Draw extends Component {
     }
   }
 
+  changeBackground = (img) => {
+    // this.setState({ imgSrc: img });
+    const canvasItems = document.getElementsByTagName('canvas')
+    const base = canvasItems[3]
+    const contextBase = base.getContext("2d");
+    const background = new Image();
+    background.crossOrigin = 'anonymous'
+    background.src = img
+    const that = this
+    background.onload = function(){
+      that.drawImageProp({ctx: contextBase, img: background})
+    }
+  }
+
+  drawImageProp = ({ctx, img, x, y, w, h, offsetX, offsetY} = {}) => {
+    // Defaults
+    if (typeof x !== "number") x = 0;
+    if (typeof y !== "number") y = 0;
+    if (typeof w !== "number") w = ctx.canvas.width;
+    if (typeof h !== "number") h = ctx.canvas.height;
+    if (typeof offsetX !== "number") offsetX = 0.5;
+    if (typeof offsetY !== "number") offsetY = 0.5;
+
+    // keep bounds [0.0, 1.0]
+    if (offsetX < 0) offsetX = 0;
+    if (offsetY < 0) offsetY = 0;
+    if (offsetX > 1) offsetX = 1;
+    if (offsetY > 1) offsetY = 1;
+
+    var iw = img.width,
+      ih = img.height,
+      r = Math.min(w / iw, h / ih),
+      nw = iw * r, // new prop. width
+      nh = ih * r, // new prop. height
+      cx,
+      cy,
+      cw,
+      ch,
+      ar = 1;
+
+    // decide which gap to fill
+    if (nw < w) ar = w / nw;
+    if (Math.abs(ar - 1) < 1e-14 && nh < h) ar = h / nh; // updated
+    nw *= ar;
+    nh *= ar;
+
+    // calc source rectangle
+    cw = iw / (nw / w);
+    ch = ih / (nh / h);
+
+    cx = (iw - cw) * offsetX;
+    cy = (ih - ch) * offsetY;
+
+    // make sure source rectangle is valid
+    if (cx < 0) cx = 0;
+    if (cy < 0) cy = 0;
+    if (cw > iw) cw = iw;
+    if (ch > ih) ch = ih;
+
+    // fill image in dest. rectangle
+    ctx.drawImage(img, cx, cy, cw, ch, x, y, w, h);
+  }
+
   componentWillReceiveProps(nextProps) {
     const { wememeContract } = nextProps;
     const { memeId, canvas } = this.state;
     let imageUrl;
 
+    const that = this
+    console.log('youoo')
     wememeContract.content.call(memeId, (e, content) => {
-      imageUrl = content;
-      // const imageUrl = "https://ipfs.infura.io/ipfs/Qmci55ieQdt8qZv5B9KuTotnsuqSUKMdmMPmjsS2x5U1pB"
-      canvas.setBackgroundImage(imageUrl, canvas.renderAll.bind(canvas), {
-        // backgroundImageOpacity: 0.5,
-        // should the image be resized to fit the container?
-        // TODO not working....??
-        backgroundImageStreftch: true,
-        opacity: 0.5,
-        width: canvas.width,
-        height: canvas.height,
-        // angle: 45,
-        // left: 400,
-        // top: 400,
-        originX: 'left',
-        originY: 'top',
-        crossOrigin: 'anonymous'
-      });
+      this.changeBackground(content)
     })
   }
 
@@ -121,15 +183,17 @@ class Draw extends Component {
 
         <div className="canvas__wrapper">
 
-          <CanvasDraw
-            ref={canvasDraw => (this.saveableCanvas = canvasDraw)}
-            brushColor={this.state.color}
-            brushRadius={this.state.brushRadius}
-            lazyRadius={this.state.lazyRadius}
-            canvasWidth={this.state.canvasWidth}
-            canvasHeight={this.state.canvasHeight}
-            imgSrc={this.state.imgSrc}
-          />
+
+            <CanvasDraw
+              ref={canvasDraw => (this.saveableCanvas = canvasDraw)}
+              brushColor={this.state.color}
+              brushRadius={this.state.brushRadius}
+              lazyRadius={this.state.lazyRadius}
+              canvasWidth={this.state.canvasWidth}
+              canvasHeight={this.state.canvasHeight}
+            />
+
+
 
 
         </div>
